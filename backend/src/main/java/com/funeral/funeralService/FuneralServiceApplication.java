7f0f220f8ed4d8ebe72271c4c51ae5207843db6f
Yurii <yvla@MacBook-Pro-Yurii.local>
2026-05-18 16:39:12 +0400

    add spring backend structure

---
+++ backend/src/main/java/com/funeral/funeralService/FuneralServiceApplication.java
@@ -0,0 +1,13 @@
+package com.funeral.funeralService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuneralServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuneralServiceApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/funeral/funeralService/FuneralServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.funeral.funeralService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuneralServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
